3654f16c6b2566fb6bed84cb9e233839cf8e6b0f
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2017-10-30 17:06:03 +0300

    src/VehicleSetup/FirmwareUpgrader.h: created interface for FirmwareUpgrader

--- src/VehicleSetup/FirmwareUpgrader.h
+++ src/VehicleSetup/FirmwareUpgrader.h
@@ -1,17 +1,46 @@
 #ifndef FIRMWAREUPGRADER_H
 #define FIRMWAREUPGRADER_H
 
-#include <QObject>
+#include <QtCore>
+
+#include "FirmwareImage.h"
 
 class FirmwareUpgrader : public QObject
 {
     Q_OBJECT
+
 public:
-    explicit FirmwareUpgrader(QObject *parent = nullptr);
+    static FirmwareUpgrader* instance(void);
+
+    virtual void start(void) = 0;
+
+    virtual void cancel(void) = 0;
+
+    virtual void reboot(void) = 0;
+
+    virtual void flash(const FirmwareImage* image) = 0;
+
+    virtual FirmwareImage* image(void) const = 0;
 
 signals:
+    void foundDevice(QString const& deviceName);
 
-public slots:
+    void deviceNotFound(void);
+
+    void error(QString const& errorString);
+
+    void upgradingStatus(QString const& status);
+
+    void upgradingCompleted(void);
+
+    void upgradingProgressChanged(uint value);
+
+protected:
+    virtual uint _getDeviceTypeIfPresent(void);
+
+    FirmwareUpgrader(QObject* parent = nullptr);
+
+    virtual ~FirmwareUpgrader(void) = 0;
 };
 
 #endif // FIRMWAREUPGRADER_H
