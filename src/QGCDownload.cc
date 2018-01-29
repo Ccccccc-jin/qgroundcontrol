0efb5493aae6450168aac179102e78b4e24f1f7e
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-01-29 15:43:55 +0300

    add QGCDownload.*: - QGCDownload and QGCDownloadWatcher classes solve several problems, such as:    - stop downloading if no internet connection    - writes on QNetworkReply::readyRead signal instead of QNetworkReply::finished    - clear interface
       QGCDownloadWatcher is QNetworkReply owner (this is adapter)
       QGCDownload class which contains factory methods fo QGCDownloadWatcher

---
+++ src/QGCDownload.cc
@@ -0,0 +1,202 @@
+#include "QGCDownload.h"
+
+// QGCDownload definition
+
+
+QNetworkAccessManager QGCDownload::_netwkManager;
+
+
+std::unique_ptr<QGCDownloadWatcher>
+    QGCDownload::download(const QString &remotePath,
+                                 std::shared_ptr<QIODevice> iodevice)
+{
+    Q_ASSERT(iodevice);
+
+    if (!iodevice->isWritable()) {
+        qWarning() << "download: iodevice not writable";
+        return {};
+
+    } else return _download(remotePath,
+        [iodevice] (QNetworkReply* reply, QGCDownloadWatcher* watcher) {
+            Q_ASSERT(reply);
+            Q_ASSERT(watcher);
+
+            auto data = reply->readAll();
+            if (iodevice->write(data) != data.size()) {
+                watcher->_onSavingError();
+            }
+        }
+    );
+}
+
+
+std::unique_ptr<QGCDownloadWatcher>
+    QGCDownload::download(const QString &remotePath,
+                                 std::shared_ptr<QByteArray> byteArray)
+{
+    Q_ASSERT(byteArray);
+
+    return _download(remotePath,
+        [byteArray] (QNetworkReply* reply, QGCDownloadWatcher* watcher) {
+            Q_UNUSED(watcher);
+            Q_ASSERT(reply);
+            Q_ASSERT(watcher);
+
+            byteArray->push_back(reply->readAll());
+        }
+    );
+}
+
+
+std::unique_ptr<QGCDownloadWatcher>
+    QGCDownload::_download(
+        QString const& remotePath,
+        std::function<void(QNetworkReply*, QGCDownloadWatcher*)> onReadyRead)
+{
+    if (remotePath.isEmpty()) {
+        qWarning() << "downloadFile empty";
+        return {};
+    }
+
+    // Split out filename from path
+    QString remoteFileName = QFileInfo(remotePath).fileName();
+    if (remoteFileName.isEmpty()) {
+        qWarning() << "Unabled to parse filename from downloadFile" << remotePath;
+        return {};
+    }
+
+    // Strip out parameters from remote filename
+    int parameterIndex = remoteFileName.indexOf("?");
+    if (parameterIndex != -1) {
+        remoteFileName  = remoteFileName.left(parameterIndex);
+    }
+
+    QUrl remoteUrl;
+    if (remotePath.startsWith("http:") || remotePath.startsWith("https:")) {
+        remoteUrl.setUrl(remotePath);
+    } else {
+        remoteUrl = QUrl::fromLocalFile(remotePath);
+    }
+    if (!remoteUrl.isValid()) {
+        qWarning() << "Remote URL is invalid" << remotePath;
+        return {};
+    }
+
+    QNetworkRequest networkRequest(remoteUrl);
+    QNetworkProxy tProxy;
+    tProxy.setType(QNetworkProxy::DefaultProxy);
+    _netwkManager.setProxy(tProxy);
+
+    auto netwkReplyRawPtr = _netwkManager.get(networkRequest);
+    auto netwkReply = std::unique_ptr<QNetworkReply>(netwkReplyRawPtr);
+
+    if (!netwkReply) {
+        qWarning() << "QNetworkAccessManager::get request failed";
+        return {};
+    }
+
+    auto watcherRawPtr = new QGCDownloadWatcher(std::move(netwkReply));
+    auto watcher = std::unique_ptr<QGCDownloadWatcher>(watcherRawPtr);
+
+    QObject::connect(&_netwkManager, &QNetworkAccessManager::networkAccessibleChanged,
+                     watcherRawPtr,  &QGCDownloadWatcher::_onNetworkAccessibilityChanged);
+
+    QObject::connect(netwkReplyRawPtr, &QNetworkReply::readyRead,
+        [netwkReplyRawPtr, onReadyRead, watcherRawPtr] (void) {
+            onReadyRead(netwkReplyRawPtr, watcherRawPtr);
+        }
+    );
+
+    return watcher;
+}
+
+
+// QGCDownloadWatcher definition
+
+QGCDownloadWatcher::QGCDownloadWatcher(QObject* parent)
+    : QObject(parent),
+      _isValid(false)
+{ }
+
+
+QGCDownloadWatcher::QGCDownloadWatcher(std::unique_ptr<QNetworkReply>&& reply,
+                                       QObject* parent)
+    : QObject(parent),
+      _isValid(true),
+      _netwkReply(std::move(reply))
+{ _initConnections(); }
+
+
+void QGCDownloadWatcher::cancel(void)
+{
+    Q_ASSERT(_netwkReply);
+
+    if (_isValid) {
+        _netwkReply->close();
+        _isValid = false;
+    } else {
+        qWarning() << "Download Watcher not valid";
+    }
+}
+
+
+void QGCDownloadWatcher::
+    _onNetworkAccessibilityChanged(QNetworkAccessManager::NetworkAccessibility access)
+{
+    Q_ASSERT(_netwkReply);
+
+    if (access == QNetworkAccessManager::NotAccessible && _isValid) {
+        qWarning() << "Network not accessible";
+        _netwkReply->disconnect();
+        cancel();
+        emit networkError(QNetworkReply::NetworkSessionFailedError);
+    }
+}
+
+
+void QGCDownloadWatcher::_onSavingError(void)
+{
+    Q_ASSERT(_netwkReply);
+
+    _netwkReply->disconnect();
+    cancel();
+    emit savingError();
+}
+
+
+void QGCDownloadWatcher::_onNetworkError(QNetworkReply::NetworkError error) {
+    Q_ASSERT(_netwkReply);
+
+    _netwkReply->disconnect();
+    if (error == QNetworkReply::OperationCanceledError) {
+        emit cancelled();
+    } else {
+        emit networkError(error);
+    }
+}
+
+
+void QGCDownloadWatcher::_onFinished(void)
+{
+    Q_ASSERT(_netwkReply);
+
+    _netwkReply->disconnect();
+    if (_netwkReply->error() == QNetworkReply::NoError) {
+        emit success();
+    }
+}
+
+
+void QGCDownloadWatcher::_initConnections(void)
+{
+    Q_ASSERT(_netwkReply);
+
+    connect(_netwkReply.get(), &QNetworkReply::downloadProgress,
+            this,              &QGCDownloadWatcher::progressChanged);
+
+    connect(_netwkReply.get(), &QNetworkReply::finished,
+            this,              &QGCDownloadWatcher::_onFinished);
+
+    connect(_netwkReply.get(), static_cast<void (QNetworkReply::*)(QNetworkReply::NetworkError)>(&QNetworkReply::error),
+            this,              &QGCDownloadWatcher::_onNetworkError);
+}
---
+++ src/QGCDownload.h
@@ -0,0 +1,85 @@
+#ifndef QGCDOWNLOAD_H
+#define QGCDOWNLOAD_H
+
+#include <QtCore>
+#include <QNetworkReply>
+#include <QNetworkProxy>
+#include <functional>
+#include <memory>
+
+class QGCDownloadWatcher;
+
+
+class QGCDownload {
+    friend class QGCDownloadWatcher;
+public:
+    /**
+     * @brief async download to object which inherits form QIODevice
+     * @param remotePath url of remote file
+     * @param iodevice iodevice which should be opened for writing
+     * @return QGCDownloadWatcher object which represents the process of downloading.
+     *         If downloading is failed, download() returns invalid pointer (nullptr)
+     */
+    static std::unique_ptr<QGCDownloadWatcher>
+        download(QString const& remotePath, std::shared_ptr<QIODevice> iodevice);
+
+    /**
+     * @brief async in memory download
+     * @param remotePath url of remote file
+     * @param byteArray storage for future data
+     * @return QGCDownloadWatcher object which represents the process of downloading.
+     *         If downloading is failed, download() returns invalid pointer (nullptr)
+     */
+    static std::unique_ptr<QGCDownloadWatcher>
+        download(QString const& remotePath, std::shared_ptr<QByteArray> byteArray);
+
+protected:
+    QGCDownload() {}
+
+private:
+    using _onReadyReadCallback_t = std::function<void(QNetworkReply*, QGCDownloadWatcher*)>;
+
+    static std::unique_ptr<QGCDownloadWatcher>
+        _download(QString const& remotePath, _onReadyReadCallback_t onReadyRead);
+
+    static QNetworkAccessManager _netwkManager;
+};
+
+
+class QGCDownloadWatcher : public QObject
+{
+    friend class QGCDownload;
+    Q_OBJECT
+public:
+    bool isValid(void) const { return _isValid; }
+
+signals:
+    void success(void);
+    void progressChanged(qint64 curr, qint64 total);
+    void networkError(QNetworkReply::NetworkError error);
+    void savingError(void);
+    void cancelled(void);
+
+public slots:
+    void cancel(void);
+
+private slots:
+    void _onNetworkAccessibilityChanged(
+            QNetworkAccessManager::NetworkAccessibility access);
+    void _onNetworkError(QNetworkReply::NetworkError error);
+    void _onSavingError(void);
+    void _onFinished(void);
+
+private:
+    QGCDownloadWatcher(QObject* parent = nullptr);
+    QGCDownloadWatcher(std::unique_ptr<QNetworkReply>&& reply,
+                       QObject* parent = nullptr);
+
+    void _initConnections(void);
+
+    bool                           _isValid;
+    std::unique_ptr<QNetworkReply> _netwkReply;
+};
+
+
+#endif // QGCDOWNLOAD_H
