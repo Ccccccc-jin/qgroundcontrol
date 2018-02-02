a8449599aaa89426f9b6e57e85c081f8b43b3c8b
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-02-02 05:18:21 +0300

    add base class FirmwareUpgradeControllerBase - base class for FirmwareUpgradeController

---
+++ src/VehicleSetup/firmware/FirmwareUpgradeControllerBase.h
@@ -0,0 +1,80 @@
+#ifndef FIRMWAREUPGRADECONTROLLERBASE_H
+#define FIRMWAREUPGRADECONTROLLERBASE_H
+
+#include <QtCore>
+#include "RemoteFirmwareInfoViewBase.h"
+
+class FirmwareUpgradeControllerBase : public QObject
+{
+    Q_OBJECT
+public:
+    virtual ~FirmwareUpgradeControllerBase() {}
+
+    enum UpdateMethod {
+        Auto, Manual
+    }; Q_ENUM(UpdateMethod)
+
+    Q_PROPERTY(QString                     firmwareVersion       READ firmwareVersion       NOTIFY firmwareVersionAvailable)
+    Q_PROPERTY(QString                     firmwareFilename      READ firmwareFilename      WRITE  setFirmwareFilename)
+    Q_PROPERTY(bool                        checksumEnabled       READ checksumEnabled       WRITE  enableChecksum)
+    Q_PROPERTY(UpdateMethod                updateMethod          READ updateMethod          WRITE  setUpdateMethod NOTIFY updateMethodChanged)
+    Q_PROPERTY(RemoteFirmwareInfoViewBase* remoteFirmwareInfo    READ remoteFirmwareInfo    NOTIFY remoteFirmwareInfoChanged)
+    Q_PROPERTY(QString                     availableDiskSpace    READ availableDiskSpace    NOTIFY availableDiskSpaceChanged)
+    Q_PROPERTY(bool                        firmwareSavingEnabled READ firmwareSavingEnabled WRITE  enableFirmwareSaving)
+
+    Q_INVOKABLE virtual void askForFirmwareFile(void) = 0;
+    Q_INVOKABLE virtual void askForFirmwareDirectory(void) = 0;
+    Q_INVOKABLE virtual bool hasEnoughDiskSpace(void) = 0;
+
+    virtual RemoteFirmwareInfoViewBase* remoteFirmwareInfo(void) = 0;
+
+    virtual QString      availableDiskSpace    (void) const = 0;
+    virtual QString      firmwareFilename      (void) const = 0;
+    virtual QString      firmwareVersion       (void) const = 0;
+    virtual UpdateMethod updateMethod          (void) const = 0;
+    virtual bool         checksumEnabled       (void) const = 0;
+    virtual bool         firmwareSavingEnabled (void) const = 0;
+
+    virtual void enableChecksum       (bool checksumEnabled) = 0;
+    virtual void enableFirmwareSaving (bool value) = 0;
+    virtual void setUpdateMethod      (UpdateMethod value) = 0;
+    virtual void setFirmwareFilename  (QString const& firmwareFilename) = 0;
+
+public slots:
+    virtual void observeDevice    (void) = 0;
+    virtual void initializeDevice (void) = 0;
+    virtual void flash            (void) = 0;
+    virtual void cancel           (void) = 0;
+
+signals:
+    void updateMethodChanged            (UpdateMethod method);
+    void firmwareVersionAvailable       (QString const& verison);
+    void remoteFirmwareVersionAvailable (QString const& verison);
+
+    void remoteFirmwareInfoChanged(void);
+    void availableDiskSpaceChanged(void);
+
+    void deviceInitializationStarted (void);
+    void firmwareUpgraderStarted     (void);
+
+    void devicePlugged   (void);
+    void deviceUnplugged (void);
+
+    void cancelled         (void);
+    void deviceInitialized (bool status);
+    void deviceFlashed     (bool status);
+
+    void flasherProgressChanged(uint progress);
+
+    void infoMsgReceived  (QString const& message);
+    void errorMsgReceived (QString const& message);
+    void warnMsgReceived  (QString const& message);
+    void firmwareInfoMsg  (QString const& message);
+
+protected:
+    explicit FirmwareUpgradeControllerBase(QObject* parent = nullptr)
+        : QObject(parent)
+    { }
+};
+
+#endif // FIRMWAREUPGRADECONTROLLERBASE_H
