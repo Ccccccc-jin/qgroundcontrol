d04ea6c486ff9aa748ff62060c78ed3c3d27fd45
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-03-10 10:27:39 +0300

    FirmwareUpgraderInterface: add macx support - use UpdateConfig for forwarding OS dependant params

--- src/VehicleSetup/firmware/FirmwareUpgraderInterface.cc
+++ src/VehicleSetup/firmware/FirmwareUpgraderInterface.cc
@@ -1,10 +1,11 @@
 #include "FirmwareUpgraderInterface.h"
-#if defined(Q_OS_WIN) || defined(Q_OS_UNIX)
+#if defined(Q_OS_WIN) || defined(Q_OS_LINUX) || defined(Q_OS_MACX)
 #include "upgraders/FirmwareUpgraderClient.h"
 #else
 #include "upgraders/FirmwareUpgraderClientStub.h"
 #endif
 
+#include "UpdateConfig.h"
 
 
 FirmwareUpgrader::FirmwareUpgrader(QObject* parent)
@@ -13,12 +14,35 @@ FirmwareUpgrader::FirmwareUpgrader(QObject* parent)
 
 
 FirmwareUpgrader::~FirmwareUpgrader(void)
-{  }
+{
+
+}
 
 
 std::unique_ptr<FirmwareUpgrader> FirmwareUpgrader::instance() {
-#if defined(Q_OS_UNIX) || defined(Q_OS_WIN)
-    return std::unique_ptr<FirmwareUpgrader>(new FirmwareUpgraderClient());
+    using Conf = UpdateConfig;
+
+#if defined(Q_OS_LINUX) || defined(Q_OS_WIN) || defined(Q_OS_MACX)
+    auto binaryPath = QCoreApplication::applicationDirPath() + "/fwupgrader";
+
+    #ifdef Q_OS_MACX
+        auto serverNodeName = "local:/tmp/fwupg_socket";
+    #else
+        auto serverNodeName = "local:fwupg_socket";
+    #endif
+
+    #ifdef Q_OS_WIN
+        binaryPath.append(".exe").replace("/", "\\");
+    #endif
+
+    auto config = UpdateConfig(
+                Conf::Vid{0x0a5c},
+                Conf::Pids{QList<int>{0x2763, 0x2764, 0x0001}},
+                Conf::ServerNodeName{serverNodeName},
+                Conf::UpdBinaryPath{binaryPath}
+    );
+
+    return std::unique_ptr<FirmwareUpgrader>(new FirmwareUpgraderClient(config));
 #else
     return std::unique_ptr<FirmwareUpgrader>(new FirmwareUpgraderClientStub());
 #endif
