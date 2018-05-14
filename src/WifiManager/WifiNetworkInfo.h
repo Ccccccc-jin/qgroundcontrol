cf0ac57ad539f1da015afc51a4c512e91da0dac6
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-05-14 16:35:13 +0300

    WifiManager: make WifiNetworkInfo
    - a container for Ssid and SecurityType

---
+++ src/WifiManager/WifiNetworkInfo.h
@@ -0,0 +1,39 @@
+#ifndef WIFINETWORKINFO_H
+#define WIFINETWORKINFO_H
+
+#include <QtCore>
+
+class WifiNetworkInfo
+{
+    Q_GADGET
+public:
+    enum SecurityType : int8_t {
+        Open = 0,
+        WEP,
+        WPA,
+        WPA2
+    };
+    Q_ENUM(SecurityType)
+
+    WifiNetworkInfo(QString ssid,
+                    SecurityType secType = SecurityType::Open)
+        : _ssid(std::move(ssid)),
+          _secType(secType)
+    { }
+
+    bool operator ==(WifiNetworkInfo const& info) { return _ssid == info._ssid; }
+    bool operator !=(WifiNetworkInfo const& info) { return !(*(this) == info); }
+
+    QString const&                ssid(void)         const { return _ssid; }
+    WifiNetworkInfo::SecurityType securityType(void) const { return _secType; }
+
+    static int ssidMaxLength(void);
+    static int passwordMaxLength(void);
+    static int passwordMinLength(void);
+
+private:
+    QString      _ssid;
+    SecurityType _secType;
+};
+
+#endif
