6da07587ee511a8f909795cfd3a0666e43c52449
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-05-15 18:06:49 +0300

    MockLink: fix ack on network deletion

--- src/comm/MockLink.cc
+++ src/comm/MockLink.cc
@@ -493,8 +493,8 @@ void MockLink::_handleWifiNetworkDelete(const mavlink_message_t &msg)
 
     mavlink_message_t wifiAck;
     mavlink_msg_wifi_ack_pack_chan(_vehicleSystemId,
-                                   msg.compid, _mavlinkChannel,
-                                   &wifiAck, msg.msgid, 0);
+                                   MAV_COMP_ID_WIFI, _mavlinkChannel,
+                                   &wifiAck, MAVLINK_MSG_ID_WIFI_NETWORK_DELETE, 0);
 
     respondWithMavlinkMessage(wifiAck);
 
